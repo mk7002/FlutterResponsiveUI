009532f69cf559d2883c46085fb0b99dc28ccd54
Milind <kenimilind864@gmail.com>
2021-11-11 17:38:18 +0530

    index_file

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -13,8 +13,8 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "71fa56ae050d01e16085c2830a5b6ced",
-"/": "71fa56ae050d01e16085c2830a5b6ced",
+"index.html": "ca6f720185cd178972774e3cdfe1a50a",
+"/": "ca6f720185cd178972774e3cdfe1a50a",
 "main.dart.js": "eca71c388727b7bce8f912389b396bb9",
 "manifest.json": "2b3f5f15a5735c0ba451c7abb70b3d50",
 "version.json": "35b885ce75c5be88dbb663ecdfe940e2"
